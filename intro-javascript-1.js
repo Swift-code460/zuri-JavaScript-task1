d86b453112931633eda966a0850d2fd686b8a885
Ahmad Abdullahi <74992166+Swift-code460@users.noreply.github.com>
2022-06-23 22:54:15 +0100

    Add files via upload

---
+++ intro-javascript-1.js
@@ -0,0 +1,9 @@
+// declare variables with values.
+let myName = "Ahmad Abdullahi";
+let height = 167;
+let country = "Nigeria";
+
+// print out the variables
+console.log("Name: " + myName);
+console.log("Height: " + height);
+console.log("Country: " + country);
