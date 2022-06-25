4af2b92db9d64f2466eee34b8436af3effd6441b
Ahmad Abdullahi <74992166+Swift-code460@users.noreply.github.com>
2022-06-25 06:41:44 +0100

    update intro-javascript-1.js

--- intro-javascript-1.js
+++ intro-javascript-1.js
@@ -4,6 +4,6 @@ let height = 167;
 let country = "Nigeria";
 
 // print out the variables
-console.log("Name: " + myName);
-console.log("Height: " + height);
-console.log("Country: " + country);
+console.log(`Name: " ${myName}
+"Height: " ${height}
+"Country: " ${country}`);
